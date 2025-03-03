e76cc727e108503c98a7c4d9a802872ab604636f
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-03-03 13:49:39 +0200

    added regestration pages, merged Login.css into Auth.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Map from "./components/Map";
+import RegistrationPage from "./components/RegistrationPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<RegistrationPage />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/map" element={<Map />} />
     </Routes>
   );
