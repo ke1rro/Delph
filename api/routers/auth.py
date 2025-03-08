d5ee4edce534217b0f8128856c238a7e3d407c18
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-03-08 23:43:57 +0200

    Iteration: 1 auth integration

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -4,6 +4,8 @@ Module for JWT authentication
 
 from core.postgres_database import database
 from fastapi import APIRouter, Depends, Form, HTTPException, status
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 from repositories.user_repo import UserRepository
 from schemas.token import TokenInfo
 from schemas.user import UserReg, UserSchema
@@ -12,6 +14,23 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from utils.utils import encode_jwt, validate_password
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router.options("/{full_path:path}")
+async def preflight(request: Request, full_path: str):
+    """
+    Handle CORS preflight requests.
+
+    Args:
+        request (Request): the request object sent by the client.
+        full_path (str): the full path of the request.
+    """
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    }
+    return JSONResponse(content=None, headers=headers, status_code=204)
 
 
 async def get_user_service(
