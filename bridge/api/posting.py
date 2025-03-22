208f254348331b7f47a87395246e5a6e13ddaa76
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2025-03-22 21:23:43 +0200

    Add ttl and removal message

--- bridge/api/posting.py
+++ bridge/api/posting.py
@@ -3,52 +3,42 @@ Posting service to send messages to the message broker.
 """
 
 from fastapi import APIRouter, Depends, Header, HTTPException
-<<<<<<< HEAD
 from pydantic import BaseModel, Field
-=======
->>>>>>> 61ea080 (Fix connection closure and logger)
 from services.queue import NoPermissionError, QueuePublishService
 from services.user import AuthenticationError, UserService
 
 from api.dependencies import get_queue_publish_service, get_user_service
-<<<<<<< HEAD
 from schemas.message import Entity, Location, Message, Velocity
-=======
-from schemas.message import Entity, Location, Velocity
->>>>>>> 61ea080 (Fix connection closure and logger)
 
 router = APIRouter(tags=["posting"])
 
 
-class CreateMessage(BaseModel):
-    """
-    Create message request schema.
-    """
-
-    entity: Entity
-    location: Location
-    velocity: Velocity | None = None
-    ttl: int | None = Field(
-        default=None,
-        ge=0,
-        le=7 * 24 * 60 * 60,
-    )
-    message_id: str | None = None
-    comment: str | None = Field(
-        default=None,
-        max_length=256,
-    )
-
-
-@router.put("/messages")
-async def create_message(
-    create_message: CreateMessage,
+@router.put("/sendMessage")
+async def send_message(
+    entity: Entity,
+    location: Location,
+    velocity: Velocity | None,
+    message_id: str | None = None,
+    comment: str | None = None,
     token: str = Header(alias="Authorization"),
     user_service: UserService = Depends(get_user_service),
     queue_service: QueuePublishService = Depends(get_queue_publish_service),
-) -> Message:
+) -> str:
     """
-    Create a new message and enqueue it.
+    Send a message to the message broker.
+
+    Args:
+        entity: The entity to be published.
+        location: The location of the entity.
+        velocity: The velocity of the entity. Defaults to None.
+        message_id: Unique message ID. Defaults to None.
+        comment: Comment for the message. Defaults to None.
+        token: User token.
+        user_service: User service dependency.
+        queue_service: Queue publish service dependency.
+
+    Returns:
+        Unique message ID.
     """
 
     # TODO: JWT authentication
@@ -58,14 +48,10 @@ async def create_message(
         raise HTTPException(status_code=403, detail="Invalid token") from e
 
     try:
-        return await queue_service.publish(
-            user,
-            create_message.entity,
-            create_message.location,
-            create_message.velocity,
-            create_message.ttl,
-            create_message.message_id,
-            create_message.comment,
-        )
+        return (
+            await queue_service.publish(
+                user, entity, location, velocity, message_id, comment
+            )
+        ).id
     except NoPermissionError as e:
         raise HTTPException(status_code=403, detail="Location is forbidden") from e
