10edb34b6ccb364b8d9bfaf8f108ac3bb72247a5
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-03-27 13:02:13 +0200

    Remove CORS middleware and preflight request handling from API

--- api/app.py
+++ api/app.py
@@ -22,11 +22,11 @@ app = FastAPI(root_path="/api/core", lifespan=lifespan)
 app.include_router(auth_router)
 app.add_middleware(AuthenticationMiddleware, backend=JWTAuthBackend())
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[settings.cors.cors_allow_origin],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-    allow_headers=["Content-Type", "Authorization"],
-    expose_headers=["Set-Cookie"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[settings.cors.cors_allow_origin],
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+#     allow_headers=["Content-Type", "Authorization"],
+#     expose_headers=["Set-Cookie"],
+# )
