1e7d04ad64b15e9b47e7239cae4be3a9bc05aee5
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2025-03-28 10:59:25 +0200

    Add basic icons to the map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,25 +1,67 @@
-import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import PageLayout from "./PageLayout";
 import BridgeClient from "../api/BridgeClient";
 import EventStorage from "../api/EventStorage";
+import ms from "milsymbol";
+
+let sidcData;
+
+async function createEventSVG(event) {
+    if(sidcData == null) {
+        sidcData = await (await fetch("/sidc.json")).json();
+    }
+
+    let sidc = sidcData.entity[event.entity.entity];
+    if (sidc == null) {
+        sidc = sidcData.entity["ground"];
+    }
+    let affiliation = sidcData.affiliation[event.entity.affiliation];
+    let status = sidcData.status[event.entity.status];
+
+    sidc = sidc.replace("@", affiliation).replace("#", status);
+
+    return new ms.Symbol(sidc).asSVG()
+}
 
 const Map = () => {
-  const storage = new EventStorage()
-  const client = new BridgeClient(storage);
+  const [markers, setMarkers] = useState([]);
 
-  storage.on("add", async (event) => {
-    console.log("Event added", event);
-  });
+  useEffect(() => {
+    const storage = new EventStorage()
+    const client = new BridgeClient(storage);
 
-  storage.on("update", async (previous_event, event) => {
-    console.log("Event updated", previous_event, event);
-  });
 
-  storage.on("remove", async (event) => {
-    console.log("Event removed", event);
-  });
+    const addMarker = async (event) => {
+      console.log("Event added", event);
+      const svgString = await createEventSVG(event);
+      const icon = L.divIcon({
+        className: "custom-icon",
+        html: `<div style="width:40px;height:40px;">${svgString}</div>`,
+        iconSize: [40, 40],
+      });
+
+      setMarkers((prevMarkers) => [
+        ...prevMarkers,
+        { position: {
+            lat: event.location.latitude,
+            lng: event.location.longitude,
+        }, icon },
+      ]);
+    };
+
+    storage.on("add", addMarker);
+
+    storage.on("update", async (previous_event, event) => {
+        console.log("Event updated", previous_event, event);
+    });
+
+    storage.on("remove", async (event) => {
+        console.log("Event removed", event);
+    });
+  }, []);
 
   return (
     <PageLayout>
@@ -33,6 +75,9 @@ const Map = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker.position} icon={marker.icon} />
+          ))}
         </MapContainer>
       </div>
     </PageLayout>
