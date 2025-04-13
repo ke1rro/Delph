eba1fc576ff6b9be918bc1b87ddaa272068eede3
yagodanr <yagodanr@gmail.com>
2025-04-13 14:34:57 +0300

    Authentication is required for history.service

--- history/api/router.py
+++ history/api/router.py
@@ -3,7 +3,9 @@ History data  service entry point.
 """
 
 from fastapi import APIRouter, Depends
+from fastapi.requests import Request
 from pydantic import BaseModel, Field
+from starlette.authentication import requires
 
 from api.dependencies import get_history_repository
 from models.event import Event
@@ -20,8 +22,11 @@ class CreateEvent(BaseModel):
     event: Event
 
 
-@router.get("/history")
-async def get_data(db: HistoryRepository = Depends(get_history_repository)):
+@router.get("/")
+@requires(["authenticated"])
+async def get_data(
+    request: Request, db: HistoryRepository = Depends(get_history_repository)
+):
     """
     Get history data.
     """
@@ -29,15 +34,17 @@ async def get_data(db: HistoryRepository = Depends(get_history_repository)):
     return await db.get_all_events()
 
 
-# @router.post("/history")
-# async def post_message(event: Event, db=Depends(get_history_repository)):
+# @router.post("")
+@requires(["authenticated"])
+# async def post_message(request: Request, event: Event, db=Depends(get_history_repository)):
 #     await db.save_event(event)
 #     return await db.get_event_by_id(event.id)
 
 
-@router.get("/history/{id}")
+@router.get("/{id}")
+@requires(["authenticated"])
 async def get_active_event_by_message_id(
-    id: str, timestamp: int, db=Depends(get_history_repository)
+    request: Request, id: str, timestamp: int, db=Depends(get_history_repository)
 ):
     """
     Get active event by message_id
@@ -45,8 +52,11 @@ async def get_active_event_by_message_id(
     return await db.get_active_event_by_message_id(id, timestamp)
 
 
-@router.get("/history/")
-async def get_active_events(timestamp: int, db=Depends(get_history_repository)):
+@router.get("/active/")
+@requires(["authenticated"])
+async def get_active_events(
+    request: Request, timestamp: int, db=Depends(get_history_repository)
+):
     """
     Get all active events
     """
