760c0000a3efdbaafe45ef3d26931da7f08d508c
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-04-15 13:11:58 +0300

    Refactor event filtering and repository methods for improved clarity and functionality; update to use message schema and enhance event retrieval logic.

--- history/api/router.py
+++ history/api/router.py
@@ -4,7 +4,7 @@ History data service API routers.
 
 from fastapi import APIRouter, Depends
 from fastapi.requests import Request
-from models.event import Event
+from schemas.message import Message
 from repositories.history import HistoryRepository
 from starlette.authentication import requires
 
@@ -14,19 +14,21 @@ router = APIRouter(prefix="/filter", tags=["filter"])
 
 
 @router.get("/events")
-@requires(["authenticated"])
 async def filter_events_by_time_stamp(
     request: Request,
     start_timestamp: int,
     end_timestamp: int,
     db: HistoryRepository = Depends(get_history_repository),
-) -> list[Event,]:
+):
     """
     Returns the events that match the timestamp
 
     Args:
         start_time (int): The start time to match the events
-        end_time (int): The end time to match the evenets
+        end_time (int): The end time to match the events
         db (HistoryRepository, optional): The mongo session
     """
-    return await db.get_events_aggregated_by_message_id(start_timestamp * 1000, end_timestamp * 1000)
+    return await db.get_events_aggregated_by_timestamp(start_timestamp * 1000, end_timestamp * 1000)
+
+
+# history          |   {'type': 'missing', 'loc': ('response', 1, 'entity'), 'msg': 'Field required', 'input': {'message': {'id': 'c6a00c6a87704fdbaee5869eed331bfc', 'timestamp': 1744711579625, 'ttl': 604800000, 'source': {'id': 'DELTA_USER/e5e54c0c35d74319a37adedcefcebfc8', 'name': 'fkdsfkj', 'comment': 'w'}, 'location': {'latitude': 45.089036, 'longitude': 4.21875, 'altitude': None, 'radius': None}, 'velocity': {'direction': 3.0, 'speed': 3.0}, 'entity': {'affiliation': 'neutral', 'entity': 'space:satellite', 'status': 'disabled'}}}}
