c3f43bdfc76787ea6e70c6eeb443780691f0eae6
Nikita Lenyk <nikitalenyk2@gmail.com>
2025-04-16 10:55:46 +0300

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/Manual.jsx
+++ frontend/src/components/Manual.jsx
@@ -10,7 +10,7 @@ const Manual = () => {
           After registration, you will receive a unique ID that you can copy only once. Take it
           through because you can only access the system with this ID.
         </p>
-        <img src="/for manual/login_page_1.png" alt="Login Page Screenshot" />
+        <img src="login_page_1.png" alt="Login Page Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -18,7 +18,7 @@ const Manual = () => {
         <p>
           Fill in your personal information to create an account. Provide your first name, last name, and create a password.
         </p>
-        <img src="/for manual/registrarion_page_2.png" alt="Registration Page Screenshot" />
+        <img src="registrarion_page_2.png" alt="Registration Page Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -26,7 +26,7 @@ const Manual = () => {
         <p>
           Upon successful registration, you will receive a unique ID. Make sure to save this ID as you will need it to log in to your account.
         </p>
-        <img src="/for manual/regestration_info_ex_3.png" alt="Registration Info Screenshot" />
+        <img src="regestration_info_ex_3.png" alt="Registration Info Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -35,7 +35,7 @@ const Manual = () => {
           Use your unique ID and password to log in to the system. Ensure you keep your credentials
           secure.
         </p>
-        <img src="/for manual/login_usr_5.png" alt="Login Screenshot" />
+        <img src="login_usr_5.png" alt="Login Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -44,7 +44,7 @@ const Manual = () => {
           The monitor bar provides real-time updates and notifications about the system's status and
           events.
         </p>
-        <img src="/for manual/monitor_main_page_6.png" alt="Monitor Bar Screenshot" />
+        <img src="monitor_main_page_6.png" alt="Monitor Bar Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -52,7 +52,7 @@ const Manual = () => {
         <p>
           The Map View button will send you to the map. Click the plus to create a new object.
         </p>
-        <img src="/for manual/map_view_7.png" alt="Map View Screenshot" />
+        <img src="map_view_7.png" alt="Map View Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -61,7 +61,7 @@ const Manual = () => {
           Here, you can see the total number of events on the map that are available to you. Among
           them are active (Active Events), destroyed (Destroyed), and neutralized (Disabled).
         </p>
-        <img src="/for manual/map_side_bar_8.png" alt="Map Sidebar Screenshot" />
+        <img src="map_side_bar_8.png" alt="Map Sidebar Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -69,7 +69,7 @@ const Manual = () => {
         <p>
           When you select an object, the sidebar will display detailed information about it.
         </p>
-        <img src="/for manual/detailed_sidebar_8.png" alt="Detailed Sidebar Screenshot" />
+        <img src="detailed_sidebar_8.png" alt="Detailed Sidebar Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -80,8 +80,8 @@ const Manual = () => {
           Then you can left-click to place an object. The coordinates will be automatically
           specified.
         </p>
-        <img src="/for manual/picking_loc_on_the_map_9.png" alt="Pick Location Screenshot" />
-        <img src="/for manual/cross_on_the_map_10.png" alt="Cursor Change Screenshot" />
+        <img src="picking_loc_on_the_map_9.png" alt="Pick Location Screenshot" />
+        <img src="cross_on_the_map_10.png" alt="Cursor Change Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -91,8 +91,8 @@ const Manual = () => {
           will see what our object on the map will look like. We create an object. The object
           appeared on the map.
         </p>
-        <img src="/for manual/obj_preview_11.png" alt="Object Preview Screenshot" />
-        <img src="/for manual/create_pbg_12.png" alt="Create Object Screenshot" />
+        <img src="obj_preview_11.png" alt="Object Preview Screenshot" />
+        <img src="create_pbg_12.png" alt="Create Object Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -102,8 +102,8 @@ const Manual = () => {
           object parameters. After adjusting the data on the object, click the "Update Event" to
           save the changes.
         </p>
-        <img src="/for manual/click_on_the_obj_12.png" alt="Click on Object Screenshot" />
-        <img src="/for manual/editing_obj_13.png" alt="Editing Object Screenshot" />
+        <img src="click_on_the_obj_12.png" alt="Click on Object Screenshot" />
+        <img src="editing_obj_13.png" alt="Editing Object Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -111,7 +111,7 @@ const Manual = () => {
         <p>
           After creating or editing an object, the map will update to reflect the changes.
         </p>
-        <img src="/for manual/main_page_after_new_obj_14.png" alt="Updated Map Screenshot" />
+        <img src="main_page_after_new_obj_14.png" alt="Updated Map Screenshot" />
       </section>
 
       <section className="manual-section">
@@ -120,7 +120,7 @@ const Manual = () => {
           By clicking on the "Profile" tab, we will open the user's profile tab, where all the
           necessary information about the user will be displayed.
         </p>
-        <img src="/for manual/profile_15.png" alt="Profile Tab Screenshot" />
+        <img src="profile_15.png" alt="Profile Tab Screenshot" />
       </section>
     </div>
   );
